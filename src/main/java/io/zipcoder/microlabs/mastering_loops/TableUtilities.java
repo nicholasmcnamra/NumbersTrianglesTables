55bf04fca9e929e2cffb49a3015624952c9245b1
nicholasmcnamra <nikomcnamara@gmail.com>
2024-03-05 18:39:46 -0500

    finshed

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -5,13 +5,18 @@ import static io.zipcoder.microlabs.mastering_loops.NumberUtilities.getRange;
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
         String table = "";
-        int [] [] matrix = new int[4][4];
+        int [] [] matrix = new int[5][5];
         int row = matrix.length;
         int column = matrix[0].length;
 
         for (int i = 1; i <=row; i++) {
             for (int j = 1; j <= column; j++) {
-                table += String.valueOf(i * j);
+                if (i * j > 9) {
+                    table += " " + i * j + " |";
+                }
+                else {
+                    table += "  " + i * j + " |";
+                }
             }
             table += "\n";
         }
@@ -19,10 +24,48 @@ public class TableUtilities {
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        String table = "";
+        int [] [] matrix = new int[10][10];
+        int row = matrix.length;
+        int column = matrix[0].length;
+
+        for (int i = 1; i <=row; i++) {
+            for (int j = 1; j <= column; j++) {
+                if (i * j >= 100) {
+                    table += "" + i * j + " |";
+                }
+                else if (i * j > 9) {
+                    table += " " + i * j + " |";
+                }
+                else {
+                    table += "  " + i * j + " |";
+                }
+            }
+            table += "\n";
+        }
+        return table;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String table = "";
+        int [] [] matrix = new int[tableSize][tableSize];
+        int row = matrix.length;
+        int column = matrix[0].length;
+
+        for (int i = 1; i <=row; i++) {
+            for (int j = 1; j <= column; j++) {
+                if (i * j >= 100) {
+                    table += "" + i * j + " |";
+                }
+                else if (i * j > 9) {
+                    table += " " + i * j + " |";
+                }
+                else {
+                    table += "  " + i * j + " |";
+                }
+            }
+            table += "\n";
+        }
+        return table;
     }
 }
