ea75af6ec54a9b194449e73d7c08d4d42ccc58ec
nicholasmcnamra <nikomcnamara@gmail.com>
2024-03-05 18:21:40 -0500

    figured out matrix and how to get multiplication table to print but need to format now

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -1,12 +1,21 @@
 package io.zipcoder.microlabs.mastering_loops;
 
+import static io.zipcoder.microlabs.mastering_loops.NumberUtilities.getRange;
+
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-//        String smallTable = "";
-//        for (int i = 1; i < 3; i++) {
-//            for (int j = 2;)
-//        }
-        return null;
+        String table = "";
+        int [] [] matrix = new int[4][4];
+        int row = matrix.length;
+        int column = matrix[0].length;
+
+        for (int i = 1; i <=row; i++) {
+            for (int j = 1; j <= column; j++) {
+                table += String.valueOf(i * j);
+            }
+            table += "\n";
+        }
+        return table;
     }
 
     public static String getLargeMultiplicationTable() {
